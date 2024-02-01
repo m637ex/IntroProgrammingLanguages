6fa409453a54baeeffef3ba2d26209d91ab5954f
m637ex <m637ex@gmail.com>
2024-02-01 23:45:14 +0700

    add lesson1:task5

---
+++ Lesson/Lesson_1/Task_5/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 5
+Дано натуральное число N. Вывести на экран числа от 1 до N.
+Пример 1
+N = 4
+1 2 3 4
+Пример 2
+N = 9
+1 2 3 4 5 6 7 8 9   */
+
+int n = 4;
+Console.WriteLine("Решение 1:");
+for (int i = 1; i <= n; i++)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+}
+
+Console.WriteLine();
+Console.WriteLine("Решение 2:");
+int j = 1;
+while (j <= n)
+{
+    Console.Write(j);
+    Console.Write(' ');
+    j++;
+}
+
