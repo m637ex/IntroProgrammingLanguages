829e49ea556a03d297f23e5fed3644d5b759067f
m637ex <m637ex@gmail.com>
2024-02-01 22:28:50 +0700

    add some variables lesson 1 / task1

--- Lesson/Lesson_1/Task_1/Program.cs
+++ Lesson/Lesson_1/Task_1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.5;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
