ce182528d32135f03fe1deb6c2373ba543f42d86
m637ex <m637ex@gmail.com>
2024-02-01 22:42:44 +0700

    add task 2

--- Lesson/Lesson_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
+++ Lesson/Lesson_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1148ffa98c80170f78d6020c057408a229ba0ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson/Lesson_1/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Console.WriteLine("Hello, World!");
+
+int i1 = 5;
+int i2 = 6;
+
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = i1 * l1;
+double product3 = i1 * d1;
+Console.WriteLine(sum);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
