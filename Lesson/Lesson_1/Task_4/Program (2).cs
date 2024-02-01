34dcdc2da63f084befa7d311d490b7079cff5c33
m637ex <m637ex@gmail.com>
2024-02-01 23:30:33 +0700

    add lesson1:task4

---
+++ Lesson/Lesson_1/Task_4/Program (2).cs
@@ -0,0 +1,25 @@
+﻿/* Задача 4
+Дано 5 гирь.
+найти вес самой тяжёлой гири */
+
+int a = 5;
+int b = 6;
+int c = 7;
+int d = 8;
+int e = 9;
+
+int max = a;
+if (b > max){
+    max = b;    
+}
+if (c > max){
+    max = c;    
+}
+if (d > max){
+    max = d;
+}
+if (e > max){
+    max = e;  
+}
+
+Console.WriteLine(max);
